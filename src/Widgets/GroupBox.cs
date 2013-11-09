7b5ce6d90b60bac347036173a05c60380612b663
Stephen Brandt <stephen@stephenbrandt.com>
2013-11-09 22:52:56 +0100

    Now using a plain VBox instead of a Frame for GroupBoxes

--- src/Widgets/GroupBox.cs
+++ src/Widgets/GroupBox.cs
@@ -23,53 +23,47 @@ using Gtk;
 
 namespace Widgets
 {
-
-    public class GroupBox : Frame
+    
+    public class GroupBox : VBox
     {
-
+        
         private VBox grpBox;
         private Label grpLabel;
         
         
         public GroupBox ( string label ) : base ()
         {
-        
-            this.ShadowType = ShadowType.None;
+            
             this.BorderWidth = 6;
             
             if ( label != "" )
             {
                 grpLabel = new Label ();
+                grpLabel.Xalign = 0;
+                grpLabel.Xpad = 6;
                 grpLabel.Ypad = 6;
                 grpLabel.Markup = String.Format ( "<b>{0}</b>", label );
                 
-                this.LabelWidget = grpLabel;
+                this.Add ( grpLabel );
             }
-
+            
             VBox emptyBox = new VBox ();
             
             grpBox = new VBox ();
             grpBox.Spacing = 6;
             
             HBox hbox = new HBox ();
-            hbox.Add ( emptyBox );
-            hbox.Add ( grpBox );
-            
-            Box.BoxChild bc1 = ( ( Box.BoxChild ) ( hbox [ emptyBox ] ) );
-            bc1.Padding = 6;
-            bc1.Expand = false;
-            
-            Box.BoxChild bc2 = ( ( Box.BoxChild ) ( hbox [ grpBox ] ) );
-            bc2.Padding = 3;
+            hbox.PackStart ( emptyBox, false, false, 6 );
+            hbox.PackStart ( grpBox, true, true, 3 );
             
             this.Add ( hbox );
-
+            
         }
         
         public void AddWidget ( Widget widget )
         {
-        
-            this.grpBox.Add ( widget );
+            
+            this.grpBox.PackStart ( widget, false, false, 0 );
             
         }
         
