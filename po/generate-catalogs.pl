28176be5279cc0a88ecbd2e8aba30569272ef70c
Stephen Brandt <stephen@stephenbrandt.com>
2014-03-30 23:11:02 +0200

    * Updated Swedish translation * Added Perl script to generate gettext message catalogs

---
+++ po/generate-catalogs.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+@locales = ('ast', 'bg', 'cs', 'de', 'es', 'fr', 'id', 'it', 'ja', 'kk', 'nl', 'pl', 'pt_BR', 'ru', 'sv', 'tr', 'uk');
+
+foreach $locale (@locales)
+{
+  system("msgfmt $locale.po -o haguichi.mo");
+  system("mv haguichi.mo ../locale/$locale/LC_MESSAGES/haguichi.mo");
+}
+
